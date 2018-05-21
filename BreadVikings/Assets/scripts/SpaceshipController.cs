8b8b894df317ef33edf7900cc7f4c71b3d735ad3
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2018-05-21 20:53:50 +0200

    first scene, spaceship movement, firts planet

---
+++ BreadVikings/Assets/scripts/SpaceshipController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpaceshipMotor))]
+public class SpaceshipController : MonoBehaviour
+{
+
+    public LayerMask movement;
+
+    Camera cam;
+    SpaceshipMotor motor;
+
+    bool enablePlant;
+    bool enablePickFruits;
+
+    GameObject plot;
+    GameObject tree;
+
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<SpaceshipMotor>();
+
+        enablePlant = false;
+        enablePickFruits = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movement))
+            {
+                motor.MoveToPoint(hit.point);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# BreadVikings
+
+Vikingos que se van al espacio e intentan volver a casa después de comprar el pan (SALE MAL)
+
+Somos Bardo y Beelzenef creando juegos e imaginando mundos, ¡nos veremos entre _commits_!
